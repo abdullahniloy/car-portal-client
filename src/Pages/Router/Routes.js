404ca0f3c575bb6a7e201a7b0ca1e7291a402034
niloyabdullah <abdullahniloy149@gmail.com>
2023-03-08 13:37:04 +0600

    2nd commit

--- src/Pages/Router/Routes.js
+++ src/Pages/Router/Routes.js
@@ -1,8 +1,29 @@
+import Main from "../../Outlet/Main";
+import Home from "../Home/Home";
+import Footer from "../Shared/Footer/Footer";
+import Header from "../Shared/Header/Header";
+
 const { createBrowserRouter } = require("react-router-dom");
 
 const router = createBrowserRouter([
     {
         path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/header',
+                element: <Header></Header>
+            },
+            {
+                path: '/home',
+                element: <Home></Home>
+            },
+            {
+                path: '/footer',
+                element: <Footer></Footer>
+            }
+        ]
+
     }
 ])
 export default router;
